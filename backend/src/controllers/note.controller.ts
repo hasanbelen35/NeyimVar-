346f3595dab0f29ab2053c7d44117fe15279bd9d
Hasan Belen <159070330+hasanbelen35@users.noreply.github.com>
2026-02-22 17:26:06 +0300

    [B] The backend code base (notes)  was converted to be compatible with OOP princibles (DI)

--- backend/src/controllers/note.controller.ts
+++ backend/src/controllers/note.controller.ts
@@ -1,61 +1,61 @@
 import { Request, Response } from 'express';
-import * as NoteService from '../services/note.service';
+import { NoteService } from '../services/note.service';
 import { catchAsync } from '../utils/catchAsync';
 import { validateUser } from '../utils/validate.user';
 
-// CREATE NOTE CONTROLLER
-export const createNoteController = catchAsync(async (req: Request, res: Response) => {
-    const userId = validateUser(req); 
-    const { title, content, isPublic } = req.body;
-
-    const newNote = await NoteService.createNoteService(userId, {
-        title,
-        content,
-        isPublic: isPublic ?? true
-    });
-    res.status(201).json({
-        status: 'success',
-        data: newNote
-    });
-});
-
-// GET ALL NOTES CONTROLLER
-export const getAllNotesController = catchAsync(async (req: Request, res: Response) => {
-    const userId = validateUser(req);
-    const notes = await NoteService.getAllNotesService(userId);
-    res.status(200).json({
-        status: 'success',
-        data: notes
-    });
-});
-
-// DELETE NOTE CONTROLLER
-export const deleteNoteController = catchAsync(async (req: Request, res: Response) => {
-    const userId = validateUser(req);
-    const noteId = req.params.id as string; 
-
-    await NoteService.deleteNoteService(noteId, userId);
+export class NoteController {
+    // get into NoteController with dependency injection
+    constructor(private noteService: NoteService) { }
     
-    res.status(200).json({
-        status: 'success',
-        message: 'Note deleted successfully.'
-    });
-});
-
-// UPDATE NOTE CONTROLLER
-export const updateNoteController = catchAsync(async (req: Request, res: Response) => {
-    const userId = validateUser(req);
-    const noteId = req.params.id as string;
-    const { title, content, isPublic } = req.body;
-
-    const updatedNote = await NoteService.updateNoteService(noteId, userId, {
-        title,
-        content,
-        isPublic: isPublic ?? true
+    // CREATE NOTE CONTROLLER
+    createNoteController = catchAsync(async (req: Request, res: Response) => {
+        const userId = validateUser(req);
+        const { title, content, isPublic } = req.body;
+        const newNote = await this.noteService.createNoteService(userId, {
+            title,
+            content,
+            isPublic: isPublic ?? true
+        });
+        res.status(201).json({
+            status: 'success',
+            data: newNote
+        });
     });
 
-    res.status(200).json({
-        status: 'success',
-        data: updatedNote
+    // GET ALL NOTES CONTROLLER
+    getAllNotesController = catchAsync(async (req: Request, res: Response) => {
+        const userId = validateUser(req);
+        const notes = await this.noteService.getAllNotesService(userId);
+        res.status(200).json({
+            status: 'success',
+            data: notes
+        });
     });
-});
+  
+    // DELETE NOTE CONTROLLER
+    deleteNoteController = catchAsync(async (req: Request, res: Response) => {
+        const userId = validateUser(req);
+        const noteId = req.params.id as string;
+        await this.noteService.deleteNoteService(noteId, userId);
+        res.status(200).json({
+            status: 'success',
+            message: 'Note deleted successfully.'
+        });
+    });
+   
+    // UPDATE NOTE CONTROLLER
+    updateNoteController = catchAsync(async (req: Request, res: Response) => {
+        const userId = validateUser(req);
+        const noteId = req.params.id as string;
+        const { title, content, isPublic } = req.body;
+        const updatedNote = await this.noteService.updateNoteService(noteId, userId, {
+            title,
+            content,
+            isPublic: isPublic ?? true
+        });
+        res.status(200).json({
+            status: 'success',
+            data: updatedNote
+        });
+    });
+}
--- backend/src/services/note.service.ts
+++ backend/src/services/note.service.ts
@@ -1,67 +1,53 @@
-/*
-id        Int      @id @default(autoincrement())
-  title     String
-  content   String   @db.Text
-  isPublic  Boolean  @default(true) 
-  createdAt DateTime @default(now())
-  updatedAt DateTime @updatedAt
-
-  userId    Int
-  user      User     
-*/
-
 import { NoteDto } from '../types/note.type';
 import prisma from '../database/db';
 
-// CREATE NOTE SERVICE
-export const createNoteService = async (userId: string, noteData: NoteDto) => {
-    return await prisma.note.create({
-        data: {
-            ...noteData,
-            userId
+export class NoteService {
+    // CREATE NOTE SERVICE
+    async createNoteService(userId: string, noteData: NoteDto) {
+        return await prisma.note.create({
+            data: {
+                ...noteData,
+                userId
+            }
+        });
+    }
+    // GET ALL NOTES SERVICE
+    async getAllNotesService(userId: string) {
+        return await prisma.note.findMany({
+            where: { userId }
+        });
+    }
+    // DELETE NOTE SERVICE
+    async deleteNoteService(noteId: string, userId: string) {
+        const note = await prisma.note.findUnique({
+            where: { id: noteId }
+        });
+        if (!note) {
+            throw new Error('Note not found');
         }
-    });
-};
-
-// GET ALL NOTES SERVICE
-export const getAllNotesService = async (userId: string) => {
-    return await prisma.note.findMany({
-        where: { userId }
-    });
-};
-
-// DELETE NOTE SERVICE
-export const deleteNoteService = async (noteId: string, userId: string) => {
-    const note = await prisma.note.findUnique({
-        where: { id: noteId }
-    });
-    if (!note) {
-        throw new Error('Note not found');
-    };
-    if (note.userId !== userId) {
-        throw new Error('Unauthorized');
-    };
-    await prisma.note.delete({
-        where: { id: noteId }
-    });
-
-};
-
-// UPDATE NOTE SERVICE
-export const updateNoteService = async (noteId: string, userId: string, updatedNoteData: NoteDto) => {
-    const note = await prisma.note.findUnique({
-        where: { id: noteId }
-    });
-    if (!note) {
-        throw new Error('Note not found');
-    };
-    if (note.userId !== userId) {
-        throw new Error('Unauthorized');
-    };
-    return await prisma.note.update({
-        where: { id: noteId },
-        data: {
-            ...updatedNoteData
+        if (note.userId !== userId) {
+            throw new Error('Unauthorized');
         }
-    });
-};
+        await prisma.note.delete({
+            where: { id: noteId }
+        });
+    }
+    // UPDATE NOTE SERVICE
+    async updateNoteService(noteId: string, userId: string, updatedNoteData: NoteDto) {
+        const note = await prisma.note.findUnique({
+            where: { id: noteId }
+        });
+        if (!note) {
+            throw new Error('Note not found');
+        }
+        if (note.userId !== userId) {
+            throw new Error('Unauthorized');
+        }
+        return await prisma.note.update({
+            where: { id: noteId },
+            data: {
+                ...updatedNoteData
+            }
+        });
+    }
+}
